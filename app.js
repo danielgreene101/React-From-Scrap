ef34960f748509a4ae98ee044e83a1f87deb43f2
DanielGreene101 <d.b.greene101@gmail.com>
2017-12-11 19:30:05 -0600

    this will be sad

---
+++ app.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+const { Map } = require('immutable')
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+class componentName extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+)(componentName);
